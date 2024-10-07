497de7213ef1304365eb1bfe63087449885b75bf
jnthhn <jonathanh321@nycstudents.net>
2024-10-07 12:19:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const students = ["Matt", "Mark", "Josephine", "Thomas"];
+//get 1 element
+//console.log(students[1]);
+//students.forEach((student) => console.log(student));
+const movie = {
+  title: "Star Wars",
+  release: 1977,
+  actors: ["Mark", "Harrison", "Carrie"],
+};
+//forEach
+movies.forEach((movie) => console.log(movie.title));
+//filter, to get a new array of movies that match criteria
+//array name DOT Array Command then arrow function that accepts singular element of array
+const newMovies = movies.filter((movie) => movie.release > 1990);
+console.log(newMovies);
+movies.filter((movie) => movie.release > 1990);
